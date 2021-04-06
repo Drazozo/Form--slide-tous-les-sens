2e457e1b15cd4cf62ac5401c0bc7b4415dbba97d
Enzo Metayer <mr.metayer.enzo@gmail.com>
2021-04-06 09:43:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Form---slide-tous-les-sens
+ (COURS) ce repo sert principalement de cours pour se rappeler du nom pour faire les défilement de texte, toujours utile d'avoir ça sous la main quand on apprends le HTML et CSS  
---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>défilement</title>
+</head>
+<body>
+
+
+C’est le mode par défaut, dans lequel le texte défile de droite à gauche.
+<marquee behavior="scroll">Le texte défile</marquee>
+
+<hr>
+Le texte va et vient à droite et à gauche.
+<marquee behavior="alternate">Le texte va et vient </marquee>
+
+<hr>
+Le texte glisse de droite à gauche et s’arrête une fois arrivé en bout de ligne.
+<marquee behavior="slide">Le texte glisse</marquee>
+
+<hr>
+Vers la droite : "right"
+<marquee direction="right">Le texte défile vers la droite </marquee>
+
+<hr>
+Vers la gauche : "left"
+<marquee direction="left">Le texte défile vers la gauche</marquee>
+
+<hr>
+Vers le haut : "up"
+<marquee direction="up">Le texte défile vers le haut</marquee>
+
+<hr>
+Vers le bas : "down"
+<marquee direction="down">Le texte défile vers le bas</marquee>
+
+<hr>
+Très lente
+<marquee scrollamount="1">Le texte se déplace très lentement</marquee>
+<hr>
+Moyenne
+<marquee scrollamount="5">Le texte se déplace un peu plus rapidement</marquee>
+
+<hr>
+Rapide
+<marquee scrollamount="10">Le texte se déplace rapidement</marquee>
+
+<hr>
+Très rapide
+<marquee scrollamount="20">Le texte se déplace très rapidement</marquee>
+
+<hr>
+Hauteur : height
+Plus le chiffre, exprimé en pixels, est élevé, plus la hauteur est importante.
+<marquee direction="up" height="20">Zone de défilement basse></marquee>
+
+<hr>
+Largeur : width
+<marquee width="50">Zone de défilement étroite</marquee>
+
+<hr>
+Il est possible d’utiliser le nom des couleurs en anglais ou leur code hexadécimal : pour la correspondance entre les deux, voir notre tableau sur les codes couleurs HTML.
+<marquee bgcolor="orange">Zone de défilement orange</marquee>
+
+</body>
+</html>
